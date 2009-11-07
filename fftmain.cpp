22845aae844200010f5e2b97f9f9172b4fbe6070
Sam Wen <xuancongwen@gmail.com>
2009-11-07 02:30:29 +0000

    Changed to taking an FFT of doubles.  Need to address overflow issues.  Frequency data width different from input?

--- fftmain.cpp
+++ fftmain.cpp
@@ -13,23 +13,23 @@
 int main(int argc, char *argv[])
 {
     long signalLength = 100;
-    short *timeDomain = (short *)calloc(sizeof(short), signalLength);
-    short *freqDomain = (short *)calloc(sizeof(short), signalLength);
+    double *timeDomain = (double *)calloc(sizeof(double), signalLength);
+    double *freqDomain = (double *)calloc(sizeof(double), signalLength);
 
     srand(0);
     for(long signalIndex = 0; signalIndex < signalLength; ++signalIndex) {
-        //*(timeDomain + signalIndex) = (rand() % USHRT_MAX) - SHRT_MAX;  // Some (hopefully) gaussian noise
+        *(timeDomain + signalIndex) = (rand() % USHRT_MAX) - SHRT_MAX;  // Some (hopefully) gaussian noise
         //*(timeDomain + signalIndex) = sin(2 * xsPI * signalIndex);
-        *(timeDomain + signalIndex) = signalIndex;
-        printf("%i\t", *(timeDomain + signalIndex));
+        //*(timeDomain + signalIndex) = signalIndex;
+        printf("%lf\t", *(timeDomain + signalIndex));
     }
 
     bool success = xsFFT(timeDomain, signalLength, freqDomain);
     
-    puts(success ? "Success!\n" : "Fail...\n");
+    puts(success ? "\n\nSuccess!\n\n" : "\n\nFail...\n\n");
     
     for(long freqIndex = 0; freqIndex < signalLength; ++freqIndex) {
-        printf("%i\t", *(freqDomain + freqIndex));
+        printf("%lf\t", *(freqDomain + freqIndex));
     }
     
     puts("\n");
