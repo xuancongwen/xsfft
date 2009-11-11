7a1b152a8b52b5a4b9928b468fecb56f031346e2
Sam Wen <xuancongwen@gmail.com>
2009-11-11 01:25:38 +0000

    Re-organized FFT header.

--- xsfft.h
+++ xsfft.h
@@ -13,38 +13,21 @@
 
 #include "xscomplex.h"
 
-///////////////////////////////////////////
-// For a given value, find the next highest
-// power of two
-///////////////////////////////////////////
+// Find the next power of two for a given number
+
 unsigned long xsNextPowerOfTwo(const unsigned long value);
 
-///////////////////////////////////////////
-// Coerces the incoming array into a radix
-// 2 array in place.
-///////////////////////////////////////////
+// Ensure power of 2 number of input
+
 void xsCoerceDataRadix2(xsComplex *data, unsigned long *dataLength);
 
-////////////////////////////////////////////////
-// FORWARD FAST FOURIER TRANSFORM
-//     Performs the FFT in place.
-////////////////////////////////////////////////
-bool xsFFT(xsComplex *data, const unsigned long dataLength);
+// FFT and IFFT
 
-//////////////////////////////////////////////////////////
-// INVERSE FAST FOURIER TRANSFORM
-//     Perform the FFT in place.
-//
-//     The result is always scaled
-//////////////////////////////////////////////////////////
+bool xsFFT(xsComplex *data, const unsigned long dataLength);
 bool xsIFFT(xsComplex *data, const unsigned long dataLength);
 
-////////////////////////////////////////////////////
-// Uses FFT interpolation to
-// produce an upsampled data array in place.
-// Each call will scale the number of data points
-// up by a factor of 2.
-////////////////////////////////////////////////////
+// Useful functions that utilize FFTs
+
 void xsUpSample(xsComplex *data, unsigned long *dataLength);
 
 #endif
